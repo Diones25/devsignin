04f54717b668b812294c06be03965e6996b66434
Diones <pereiradiones987@gmail.com>
2023-10-17 14:16:06 -0300

    adicionando shemas, dtos e validações, e configs de modulos

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema } from './schemas/users.schema';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      }
+    ]),
+    AuthModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, AuthService]
 })
 export class UsersModule {}
